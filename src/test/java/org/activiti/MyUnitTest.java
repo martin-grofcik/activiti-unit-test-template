7bbdf99c6675e20c150680078375835404852e50
martin.grofcik <martin.grofcik@edorasware.com>
2015-04-22 09:33:44 +0200

    recursion example

--- src/test/java/org/activiti/MyUnitTest.java
+++ src/test/java/org/activiti/MyUnitTest.java
@@ -1,4 +1,5 @@
 package org.activiti;
+
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.activiti.engine.test.ActivitiRule;
@@ -6,6 +7,11 @@ import org.activiti.engine.test.Deployment;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class MyUnitTest {
@@ -16,11 +22,14 @@ public class MyUnitTest {
 	@Test
 	@Deployment(resources = {"org/activiti/test/my-process.bpmn20.xml"})
 	public void test() {
-		ProcessInstance processInstance = activitiRule.getRuntimeService().startProcessInstanceByKey("my-process");
+		Map<String, Object> vars = new HashMap<String, Object>();
+		vars.put("counter", 1);
+		ProcessInstance processInstance = activitiRule.getRuntimeService().startProcessInstanceByKey("my-process", vars);
 		assertNotNull(processInstance);
 		
-		Task task = activitiRule.getTaskService().createTaskQuery().singleResult();
-		assertEquals("Activiti is awesome!", task.getName());
+		List<Task> tasks = activitiRule.getTaskService().createTaskQuery().list();
+		assertThat(tasks.size(), is(1));
+		assertEquals("Activiti is awesome 10!", tasks.get(0).getName());
 	}
 
 }
