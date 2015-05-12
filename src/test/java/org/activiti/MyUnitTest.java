8cfbf37f30f5c5735d7a0de07970c0bfff64d3bf
Martin Grofcik <martin.grofcik@edorasware.com>
2015-05-12 08:21:29 +0200

    fixing jUnit test

--- src/test/java/org/activiti/MyUnitTest.java
+++ src/test/java/org/activiti/MyUnitTest.java
@@ -1,26 +1,32 @@
 package org.activiti;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.activiti.engine.HistoryService;
+import org.activiti.engine.history.HistoricProcessInstance;
 import org.activiti.engine.runtime.ProcessInstance;
-import org.activiti.engine.task.Task;
 import org.activiti.engine.test.ActivitiRule;
 import org.activiti.engine.test.Deployment;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class MyUnitTest {
-	
+
 	@Rule
 	public ActivitiRule activitiRule = new ActivitiRule();
-	
+
 	@Test
-	@Deployment(resources = {"org/activiti/test/my-process.bpmn20.xml"})
-	public void test() {
-		ProcessInstance processInstance = activitiRule.getRuntimeService().startProcessInstanceByKey("my-process");
+	@Deployment(resources = {"org/activiti/test/parent.bpmn20.xml", "org/activiti/test/child.bpmn20.xml"})
+	public void test_subprocess() throws Exception {
+		ProcessInstance processInstance = activitiRule.getRuntimeService().startProcessInstanceByKey("parent");
+		Thread.sleep(5000);
+
+		HistoryService historyService = activitiRule.getHistoryService();
+		HistoricProcessInstance historicProcessInstance = historyService.createHistoricProcessInstanceQuery().processInstanceId(processInstance.getId()).singleResult();
+		System.out.println("process instance end time: " + historicProcessInstance.getEndTime());
+
+		assertNotNull(historicProcessInstance.getEndTime());
 		assertNotNull(processInstance);
-		
-		Task task = activitiRule.getTaskService().createTaskQuery().singleResult();
-		assertEquals("Activiti is awesome!", task.getName());
 	}
 
 }
