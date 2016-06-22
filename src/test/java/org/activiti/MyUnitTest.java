2816a169f89992ba8f4418c654371f8fbff9fd25
Martin Grofcik <martin.grofcik@edorasware.com>
2016-06-22 08:42:42 +0200

    querry error jUnit test

--- src/test/java/org/activiti/MyUnitTest.java
+++ src/test/java/org/activiti/MyUnitTest.java
@@ -1,26 +1,37 @@
 package org.activiti;
+
+import org.activiti.engine.TaskService;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.activiti.engine.test.ActivitiRule;
-import org.activiti.engine.test.Deployment;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class MyUnitTest {
-	
+
 	@Rule
 	public ActivitiRule activitiRule = new ActivitiRule();
-	
+
 	@Test
-	@Deployment(resources = {"org/activiti/test/my-process.bpmn20.xml"})
 	public void test() {
 		ProcessInstance processInstance = activitiRule.getRuntimeService().startProcessInstanceByKey("my-process");
 		assertNotNull(processInstance);
-		
+
 		Task task = activitiRule.getTaskService().createTaskQuery().singleResult();
 		assertEquals("Activiti is awesome!", task.getName());
 	}
 
+	@Test
+	public void queryTest() {
+		TaskService taskService = this.activitiRule.getTaskService();
+
+		assertEquals(
+				taskService.createTaskQuery().includeProcessVariables().active().list().size()
+				,
+				taskService.createTaskQuery().active().list().size());
+
+	}
 }
